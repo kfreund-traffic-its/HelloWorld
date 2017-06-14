232732c8160d962274b92da97370728e63f9d9c4
kfreund-traffic-its <kfreund@traffic-its.com>
2017-06-14 16:58:35 +0200

    Initial commit

---
+++ src/MainHallo.java
@@ -0,0 +1,8 @@
+public class MainHallo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        MainHallo2.duda("hallo Duda");
+        MainHallo2.duda2("hallo Duda 2", 5);
+    }
+}
---
+++ src/MainHallo2.java
@@ -0,0 +1,16 @@
+/**
+ * Created by kfreund on 14.06.2017.
+ */
+public class MainHallo2 {
+
+    public static void duda(String zeile) {
+        System.out.println(zeile);
+    }
+
+    public static void duda2(String zeile, int schleife) {
+        for (int i = 1; i <= schleife; i++) {
+            System.out.println("--> " + zeile + " <--");
+        }
+    }
+
+}
